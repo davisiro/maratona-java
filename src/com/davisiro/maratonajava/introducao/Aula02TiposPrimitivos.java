35c51c9e535dc3e1d85436b0bd0855214cbdd415
davisiro <davisilva.rdrg@outlook.com>
2025-09-05 05:25:08 -0300

    fix: corrigir plural de 'ano' para 'anos' na mensagem

--- src/com/davisiro/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/com/davisiro/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,6 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, flot, char, byte, short, long, boolean
         int idade = 10;
-        System.out.println("A idade é "+idade+" ano");
+        System.out.println("A idade é "+idade+" anos");
     }
 }
